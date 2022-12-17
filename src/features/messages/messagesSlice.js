4b01b21c74a0bb0a023594da526344b1dfd001d1
sayeef-rahman <sayeef.cse@gmail.com>
2022-12-17 16:55:04 +0600

    apislice & store setup completd

---
+++ src/features/messages/messagesSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const messagesSlice = createSlice({
+  name: "messages",
+  initialState,
+  reducers: {},
+});
+
+export const {} = messagesSlice.actions;
+export default messagesSlice.reducer;
